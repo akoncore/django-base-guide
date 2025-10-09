a1aa44000a854fc362fe96f8b0631319baf4a2cf
пуауа <your_email@whatever.com>
2025-10-10 02:08:41 +0500

    running shop

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -14,6 +15,10 @@ class Category(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
+    
     
 
 class Product(models.Model):
@@ -36,5 +41,6 @@ class Product(models.Model):
         return self.name
     
 
-    
+    def get_absolute_url(self):
+        return reverse("main:product_detail", args=[self.id,self.slug])
     
