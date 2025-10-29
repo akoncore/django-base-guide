cee17f8fcdf4b67f36e0b1250d0f45217cc4c0f2
aconcore <akniet.talgat.05@mail.ru>
2025-10-29 22:58:45 +0500

    add ORM functions

--- main/views.py
+++ main/views.py
@@ -21,7 +21,7 @@ def product_list(request,category_slug = None):
 def product_detail(request,id,slug):
     product = get_object_or_404(Product,id=id,slug=slug,available=True)
     related_product = Product.objects.filter(category = product.category,
-                                             available=True).exclude(id=product.id)[:4]
+                                             available=True).exclude(id=product.id)[:5]
     
     cart_product_form = CartAddProductForm()
     
